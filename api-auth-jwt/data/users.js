cfb408339882136f9f7bdc0c248e6d897c8a786d
samyr <sema1@aluno.ifal.edu.br>
2026-03-18 14:28:04 -0300

    save 3

--- api-auth-jwt/data/users.js
+++ api-auth-jwt/data/users.js
@@ -1,2 +1,26 @@
-const users = 
-module.exports = users
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: { isEmail: true }
+  },
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = User;
